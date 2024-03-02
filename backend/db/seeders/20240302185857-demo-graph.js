a070a4b1dbd9608826509b35abb713af149fa68f
jandos0492 <jandos0492@gmail.com>
2024-03-02 14:15:58 -0500

    filled the seeder file for the graph table with data

---
+++ backend/db/seeders/20240302185857-demo-graph.js
@@ -0,0 +1,127 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Graphs", [
+      {
+        no: 52,
+        name: 'graph welcome',
+        video: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/graph-welcome.mp4'
+      },
+      {
+        no: 53,
+        name: 'graph intro',
+        video: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/graph-intro.mp4'
+      },
+      {
+        no: 54,
+        name: 'graph warm up',
+        video: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/graph-warm-up.mp4'
+      },
+      {
+        no: 55,
+        name: 'has path',
+        approachVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/has-path-approach.mp4',
+        walkthroughVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/has-path-walkthrough.mp4'
+      },
+      {
+        no: 56,
+        name: 'undirected path',
+        approachVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/undirected-path-approach.mp4',
+        walkthroughVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/undirected-path-walkthrough.mp4'
+      },
+      {
+        no: 57,
+        name: 'connected components count',
+        approachVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/connected-components-count-approach.mp4',
+        walkthroughVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/connected-components-count-walkthrough.mp4'
+      },
+      {
+        no: 58,
+        name: 'largest component',
+        approachVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/largest-component-approach.mp4',
+        walkthroughVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/largest-component-walkthrough.mp4'
+      },
+      {
+        no: 59,
+        name: 'slow and steady',
+        video: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/slow-and-steady.mp4',
+      },
+      {
+        no: 60,
+        name: 'shortest path',
+        approachVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/shortest-path-approach.mp4',
+        walkthroughVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/shortest-path-walkthrough.mp4'
+      },
+      {
+        no: 61,
+        name: 'island count',
+        approachVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/island-count-approach.mp4',
+        walkthroughVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/island-count-walkthrough.mp4'
+      },
+      {
+        no: 62,
+        name: 'minimum island',
+        approachVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/minimum-island-approach.mp4',
+        walkthroughVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/minimum-island-walkthrough.mp4'
+      },
+      {
+        no: 63,
+        name: 'closest carrot',
+        approachVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/closest-carrot-approach.mp4',
+        walkthroughVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/closest-carrot-walkthrough.mp4'
+      },
+      {
+        no: 64,
+        name: 'longest path',
+        approachVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/longest-path-approach.mp4',
+        walkthroughVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/longest-path-walkthrough.mp4'
+      },
+      {
+        no: 65,
+        name: 'semesters required',
+        approachVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/semesters-required-approach.mp4',
+        walkthroughVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/semesters-required-walkthrough.mp4'
+      },
+      {
+        no: 66,
+        name: 'best bridge',
+        approachVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/best-bridge-approach.mp4',
+        walkthroughVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/best-bridge-walkthrough.mp4'
+      },
+      {
+        no: 67,
+        name: 'has cycle',
+        approachVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/has-cycle-approach.mp4',
+        walkthroughVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/has-cycle-walkthrough.mp4'
+      },
+      {
+        no: 68,
+        name: 'prereqs possible',
+        approachVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/prereqs-possible-approach.mp4',
+        walkthroughVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/prereqs-possible-walkthrough.mp4'
+      },
+      {
+        no: 69,
+        name: 'knight attack',
+        approachVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/knight-attack-approach.mp4',
+        walkthroughVideo: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/knight-attack-walkthrough.mp4'
+      },
+      {
+        no: 70,
+        name: 'graph wrap up',
+        video: 'https://structy-videos.s3.us-east-2.amazonaws.com/graph/graph-wrap-up.mp4'
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
--- backend/test.js
+++ backend/test.js
@@ -1,8 +1,10 @@
-const array = ["binary tree welcome", "binary tree intro", "binary tree warm up", "depth first values", "breadth first values", "tree sum", "tree includes", "tree min value", "max root to leaf path sum", "tree path finder", "tree value count", "how high", "bottom right value", "all tree pahts", "level averages", "leaf list", "binary tree wrap up"]
+const array = ["graph welcome", "graph intro", "graph warm up", "has path", "undirected path", "connected components count", "largest component", "slow and steady", "shortest path", "island count", "minimum island", "closest carrot", "longest path", "semesters required", "best bridge", "has cycle", "prereqs possible", "knight attack", "graph wrap up"]
+// console.log(array.length);
+
 
 
 const checkVideoString = (s) => {
-    return s.includes("welcome") || s.includes("intro") || s.includes("pep talk") || s.includes("wrap up") || s.includes("warm up");
+    return s.includes("welcome") || s.includes("intro") || s.includes("pep talk") || s.includes("wrap up") || s.includes("warm up") || s.includes("slow and steady");
 }
 
 const createSeeder = (array) => {
@@ -10,14 +12,14 @@ const createSeeder = (array) => {
 
     array.forEach((elem, index) => {
         const obj = {};
-        obj.no = index + 35;
+        obj.no = index + 52;
         obj.name = elem;
         const videoName = elem.split(" ").join("-");
         if (checkVideoString(elem)) {
-            obj.video = `https://structy-videos.s3.us-east-2.amazonaws.com/binary-tree/${videoName}.mp4`;
+            obj.video = `https://structy-videos.s3.us-east-2.amazonaws.com/graph/${videoName}.mp4`;
         } else {
-            obj.approachVideo = `https://structy-videos.s3.us-east-2.amazonaws.com/binary-tree/${videoName}-approach.mp4`;
-            obj.walkthroughVideo = `https://structy-videos.s3.us-east-2.amazonaws.com/binary-tree/${videoName}-walkthrough.mp4`;
+            obj.approachVideo = `https://structy-videos.s3.us-east-2.amazonaws.com/graph/${videoName}-approach.mp4`;
+            obj.walkthroughVideo = `https://structy-videos.s3.us-east-2.amazonaws.com/graph/${videoName}-walkthrough.mp4`;
         }
         result.push(obj);
     });
