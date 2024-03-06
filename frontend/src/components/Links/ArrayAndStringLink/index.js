4d8e1352a6f8db96d1991fa93ae8c19677e7233a
jandos0492 <jandos0492@gmail.com>
2024-03-06 14:22:20 -0500

    Revised the links name for the IntroductionLink component and ArrayAndStringLink component

--- frontend/src/components/Links/ArrayAndStringLink/index.js
+++ frontend/src/components/Links/ArrayAndStringLink/index.js
@@ -4,7 +4,7 @@ import "../Links.css";
 
 const ArrayAndStringLink = ({ arrayAndString, isVisible }) => {
     return isVisible ? (
-        <Link to={`/${arrayAndString.name}/${arrayAndString.id}`} className="link">
+        <Link to={`/array-and-string/${arrayAndString.id}`} className="link">
             <div className="info">
                 <h4 className="no">{arrayAndString.no}</h4>
                 <h2 className="name">{arrayAndString.name}</h2>
--- frontend/src/components/Links/IntroductionLink/index.js
+++ frontend/src/components/Links/IntroductionLink/index.js
@@ -4,7 +4,7 @@ import "../Links.css";
 
 const IntroductionLink = ({ introduction, isVisible }) => {
     return isVisible ? (
-        <Link to={`/${introduction.name}/${introduction.id}`} className="link">
+        <Link to={`/introduction/${introduction.id}`} className="link">
             <div className="info">
                 <h4 className="no">{introduction.no}</h4>
                 <h2 className="name">{introduction.name}</h2>
