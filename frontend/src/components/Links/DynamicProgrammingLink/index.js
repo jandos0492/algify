b3728a1ca986ab7866e8335b57804f00aa9a4862
jandos0492 <jandos0492@gmail.com>
2024-03-09 17:42:59 -0500

    changed the Link to Navlink for DynamicProgramming component

--- frontend/src/components/Links/DynamicProgrammingLink/index.js
+++ frontend/src/components/Links/DynamicProgrammingLink/index.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../Links.css";
 
 const DynamicProgrammingLink = ({ dynamicProgramming, isVisible }) => {
     return isVisible ? (
-        <Link to={`/dynamic-programming/${dynamicProgramming.id}`} className="link">
+        <NavLink
+            to={`/dynamic-programming/${dynamicProgramming.id}`}
+            className="link"
+            style={({ isActive }) => isActive ? { backgroundColor: "#21fd58" } : {}}
+        >
             <div className="info">
                 <h4 className="no">{dynamicProgramming.no}</h4>
                 <h2 className="name">{dynamicProgramming.name}</h2>
             </div>
-        </Link>
+        </NavLink>
     ) : null
 };
 
