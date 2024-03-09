41a30a148ed6f4d47704afd4464dd7036b76ab02
jandos0492 <jandos0492@gmail.com>
2024-03-09 17:52:47 -0500

    changed the Link to Navlink for MixedRecallLink component

--- frontend/src/components/Links/MixedRecallLink/index.js
+++ frontend/src/components/Links/MixedRecallLink/index.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../Links.css";
 
 const MixedRecallLink = ({ mixedRecall, isVisible }) => {
     const { no, name } = mixedRecall;
     return isVisible ? (
-        <Link to={`/mixed-recall/${mixedRecall.id}`} className="link">
+        <NavLink
+            to={`/mixed-recall/${mixedRecall.id}`}
+            className="link"
+            style={({ isActive }) => isActive ? { backgroundColor: "#21fd58" } : {}}
+        >
             <div className="info">
                 <h4 className="no">{no}</h4>
                 <h2 className="name">{name}</h2>
             </div>
-        </Link>
-    ): null
+        </NavLink>
+    ) : null
 };
 
 export default MixedRecallLink;
