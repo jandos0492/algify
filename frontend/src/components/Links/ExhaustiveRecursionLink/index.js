fa829a293c8007abf668a2b04ec3987867c4c54a
jandos0492 <jandos0492@gmail.com>
2024-03-09 17:44:44 -0500

    changed the Link to Navlink for ExhaustiveRecursionLink component

--- frontend/src/components/Links/ExhaustiveRecursionLink/index.js
+++ frontend/src/components/Links/ExhaustiveRecursionLink/index.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../Links.css";
 
 const ExhaustiveRecursionLink = ({ exhaustiveRecursion, isVisible }) => {
     return isVisible ? (
-        <Link to={`/exhaustive-recursion/${exhaustiveRecursion.id}`} className="link">
+        <NavLink
+            to={`/exhaustive-recursion/${exhaustiveRecursion.id}`}
+            className="link"
+            style={({ isActive }) => isActive ? { backgroundColor: "#21fd58" } : {}}
+        >
             <div className="info">
                 <h4 className="no">{exhaustiveRecursion.no}</h4>
                 <h2 className="name">{exhaustiveRecursion.name}</h2>
             </div>
-        </Link>
+        </NavLink>
     ) : null
 };
 
