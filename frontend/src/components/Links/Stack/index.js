8cd49cc9098f52b2ed0e632d253adf012c625286
jandos0492 <jandos0492@gmail.com>
2024-03-09 17:54:34 -0500

    changed the Link to Navlink for StackLink component

--- frontend/src/components/Links/Stack/index.js
+++ frontend/src/components/Links/Stack/index.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../Links.css";
 
 const StackLink = ({ stack, isVisible }) => {
     return isVisible ? (
-        <Link to={`/stack/${stack.id}`} className="link">
+        <NavLink 
+            to={`/stack/${stack.id}`} 
+            className="link"
+            style={({ isActive }) => isActive ? { backgroundColor: "#21fd58" } : {}}
+        >
             <div className="info">
                 <h4 className="no">{stack.no}</h4>
                 <h2 className="name">{stack.name}</h2>
             </div>
-        </Link>
+        </NavLink>
     ) : null
 };
 
