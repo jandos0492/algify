a2516e6364d8339ca4d71d2e53e16130903afa7b
jandos0492 <jandos0492@gmail.com>
2024-03-09 17:51:06 -0500

    changed the Link to Navlink for LinkedListLink component

--- frontend/src/components/Links/LinkedListLink/index.js
+++ frontend/src/components/Links/LinkedListLink/index.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../Links.css";
 
 const LinkedListLink = ({ linkedList, isVisible }) => {
     return isVisible ? (
-        <Link to={`/linked-list/${linkedList.id}`} className="link">
+        <NavLink
+            to={`/linked-list/${linkedList.id}`}
+            className="link"
+            style={({ isActive }) => isActive ? { backgroundColor: "#21fd58" } : {}}
+        >
             <div className="info">
                 <h4 className="no">{linkedList.no}</h4>
                 <h2 className="name">{linkedList.name}</h2>
             </div>
-        </Link>
+        </NavLink>
     ) : null
 };
 
