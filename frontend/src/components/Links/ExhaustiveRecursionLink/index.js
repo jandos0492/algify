20666567270af8bc670ae8e076c63eee993c74ff
jandos0492 <jandos0492@gmail.com>
2024-03-08 23:41:25 -0500

    Implemented the ExhaustiveRecursionLink compoenent, need to update the Home compoenent, to get the data

---
+++ frontend/src/components/Links/ExhaustiveRecursionLink/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../Links.css";
+
+const ExhaustiveRecursionLink = ({ exhaustiveRecursion, isVisible }) => {
+    return isVisible ? (
+        <Link to={`/exhaustive-recursion/${exhaustiveRecursion.id}`}>
+            <div className="info">
+                <h4 className="no">{exhaustiveRecursion.no}</h4>
+                <h2 className="name">{exhaustiveRecursion.name}</h2>
+            </div>
+        </Link>
+    ) : null
+};
+
+export default ExhaustiveRecursionLink;
