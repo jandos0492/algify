d75c59fbf9d7d4b02235b9e60e2a17dff769cbc4
jandos0492 <jandos0492@gmail.com>
2024-03-09 17:40:48 -0500

    changed the Link to Navlink for BinaryTree component

--- frontend/src/components/Links/BinaryTreeLink/index.js
+++ frontend/src/components/Links/BinaryTreeLink/index.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../Links.css";
 
 const BinaryTreeLink = ({ binaryTree, isVisible }) => {
     return isVisible ? (
-        <Link to={`/binary-tree/${binaryTree.id}`} className="link">
+        <NavLink
+            to={`/binary-tree/${binaryTree.id}`}
+            className="link"
+            style={({ isActive }) => isActive ? { backgroundColor: "#21fd58" } : {}}
+        >
             <div className="info">
                 <h4 className="no">{binaryTree.no}</h4>
                 <h2 className="name">{binaryTree.name}</h2>
             </div>
-        </Link>
+        </NavLink>
     ) : null;
 };
 
