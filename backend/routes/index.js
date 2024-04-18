24b0d48ba054ec54812026dabbe4967f1b181aa1
jandos0492 <jandos0492@gmail.com>
2024-04-18 14:36:26 -0400

    Updated the api/reset-password

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,6 +10,7 @@ const apiDynamicProgrammings = require("./api/dynamic-programming-router");
 const apiStacks = require("./api/stack-router");
 const apiExhaustiveRecursions = require("./api/exhaustive-recursion-router");
 const apiMixedRecalls = require("./api/mixed-recall-router");
+const apiResetPassword = require("./api/reset-password");
 
 router.use("/api", apiRouter);
 router.use("/api", apiIntroductions);
@@ -21,6 +22,7 @@ router.use("/api", apiDynamicProgrammings);
 router.use("/api", apiStacks);
 router.use("/api", apiExhaustiveRecursions);
 router.use("/api", apiMixedRecalls);
+router.use("/api", apiResetPassword);
 
 // Static routes
 // Serve React build files in produciton
